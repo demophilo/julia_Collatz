933fd4849b45cd1fad02cce55a8392cab34f7f4c
Karl Linek <karl@linek.at>
2024-11-02 17:26:28 +0100

    added function collatz_operator

--- src/julia_collatz.jl
+++ src/julia_collatz.jl
@@ -1,5 +1,17 @@
 module julia_collatz
 
-greet() = print("Hello World!")
+export collatz_operator
+
+function collatz_operator(zahl::UInt128)::UInt128
+	n = 0
+	while (zahl & 1) == 0
+		n += 1
+		zahl >>= 1
+	end
+	return ((zahl << 1) + zahl + 1) << n
+end
 
 end # module julia_collatz
+
+
+
